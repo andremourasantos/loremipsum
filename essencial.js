49d675801e9d0c38057fdb25e0b13dec9e726668
André Moura Santos <contato@andremourasantos.com>
2022-08-26 19:43:38 -0300

    ALPHA 1.3
    - Adicionado atalho de teclado.
    - Adicionado novas interações para aumentar ou diminuir a de quantidade de texto.
    - Adicionados popups de boas-vindas e novidades da versão.
    - Corrigido bug de alternância incorreta de cor da barra do navegador com o tema da página.
    - Criação de um objeto para melhor organizar as informações da ferramenta (utilizadas pelio essencial.js).
    - Ferramenta em estado de uso.
    - Finalizada responsividade.
    - Finalizado conteúdo dos popups e da página.
    - Finalizado script da página.

--- essencial.js
+++ essencial.js
@@ -3,20 +3,50 @@ window.addEventListener('DOMContentLoaded', () => {
     /*REFERÊNCIA DE TEMA*/
     if(localStorage.getItem('temaDaPagina') != null){alterarTema(0)} else{localStorage.setItem('temaDaPagina', 'Claro')}})
 
+//↓↓ EVENTOS PÓS CARRAGAMENTO
+window.addEventListener('load', ()=>{
+    /*CHECA ACESSOS*/
+    if(this.localStorage.getItem(`acessos${FERRAMENTA.nome}`) == null){this.localStorage.setItem(`acessos${FERRAMENTA.nome}`,1)}else{this.localStorage.setItem(`acessos${FERRAMENTA.nome}`, Number(this.localStorage.getItem(`acessos${FERRAMENTA.nome}`)) + 1)}
+
+    /*CHECA VERSAO*/
+    if(this.localStorage.getItem(`v${FERRAMENTA.nome}`) == null){this.localStorage.setItem(`v${FERRAMENTA.nome}`, FERRAMENTA.versao);abrirPopup('#popup_BoasVindas');setTimeout(() => {fecharPopup('#popup_BoasVindas')}, 2000);} else if(this.localStorage.getItem(`v${FERRAMENTA.nome}`) != FERRAMENTA.versao){this.localStorage.setItem(`v${FERRAMENTA.nome}`, FERRAMENTA.versao); abrirPopup('#popup_log_novidades');}
+})
+
+//↓↓ ESTILO DA PÁGINA
 function alterarTema(interacao=0){
-    let icone = document.querySelector("#alteraTemaPagina > i")
+    let icone = document.querySelector("#alteraTemaPagina > i")    
 
-    if(localStorage.getItem('temaDaPagina') === 'Escuro'){
-        if(interacao === 1){setTimeout(() => {
-            document.body.classList.remove('modo_escuro'); localStorage.setItem('temaDaPagina', 'Claro'); icone.className = 'ph-sun-fill'; icone.style.transform = 'scaleX(1)';
-        }, 125);} else {document.body.classList.add('modo_escuro'); icone.className = 'ph-moon-fill'; icone.style.transform = 'scaleX(-1)';}}
+    if(interacao===0){
+        switch (localStorage.getItem('temaDaPagina')) {
+            case 'Escuro':
+                document.body.classList.add('modo_escuro'); icone.className = 'ph-moon-fill'; icone.style.transform = 'scaleX(-1)';
+                break;
         
-        else {if(interacao ===1){setTimeout(() => {
-            document.body.classList.add('modo_escuro'); localStorage.setItem('temaDaPagina', 'Escuro'); icone.className = 'ph-moon-fill'; icone.style.transform = 'scaleX(-1)';
-        }, 125);} else {document.body.classList.remove('modo_escuro'); icone.className = 'ph-sun-fill'; icone.style.transform = 'scaleX(1)';}
+            default:
+                document.body.classList.remove('modo_escuro'); icone.className = 'ph-sun-fill'; icone.style.transform = 'scaleX(1)';
+                break;
+        }
+    } else {
+        switch (localStorage.getItem('temaDaPagina')) {
+            case 'Escuro':
+                document.body.classList.remove('modo_escuro'); localStorage.setItem('temaDaPagina', 'Claro'); icone.className = 'ph-sun-fill'; icone.style.transform = 'scaleX(1)';
+                break;
+        
+            default:
+                document.body.classList.add('modo_escuro'); localStorage.setItem('temaDaPagina', 'Escuro'); icone.className = 'ph-moon-fill'; icone.style.transform = 'scaleX(-1)';
+                break;
+        }
     }
-
-    document.querySelectorAll('meta').forEach(meta => {if(meta.name === 'theme-color'){meta.content = localStorage.getItem('temaDaPagina') === 'Escuro' ? '#333333' : '#FDFBEE'}})
+    
+    switch(localStorage.getItem('temaDaPagina')){
+        case 'Escuro':
+            document.querySelectorAll('meta').forEach(meta => {if(meta.name === 'theme-color'){meta.content = '#333333'}})
+            break;
+        
+        default:
+            document.querySelectorAll('meta').forEach(meta => {if(meta.name === 'theme-color'){meta.content = '#FDFBEE'}})
+            break;
+    }
 }
 
 //↓↓ POPUPS
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//↓↓ INFO DA FERRAMENTA
+const FERRAMENTA = {
+    nome: 'LoremIpsum',
+    versao: 'ALPHA 1.3',
+}
+
+//↓↓ CONFIGURAÇÕES
 const texto = [
     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab incidunt dolores sint velit! Non odio vel quam suscipit consectetur corrupti. Ea numquam neque culpa similique, ab expedita assumenda non ipsa unde nihil, autem doloribus earum reiciendis tempora.',
     'Accusamus eaque voluptate, minus dolorem quia, fugit atque minima consequuntur qui at vel magnam quo assumenda inventore cumque, neque quaerat incidunt. Sunt iusto quasi explicabo asperiores ad magni, amet odio. Temporibus dolorem voluptatum ipsum. Suscipit totam tempora nesciunt voluptas, quam ut optio autem vel consequuntur!',
@@ -10,27 +17,50 @@ const texto = [
     'Fugit, officia consectetur voluptatem, est recusandae illo vel repellat, voluptatibus magni dolores amet aspernatur nemo. Vero molestiae odit unde, debitis, consectetur incidunt pariatur quidem velit ipsam voluptas iure veniam dolor quibusdam ab illum, aliquam error voluptatum labore perspiciatis quam.',
     'Consequatur, veniam numquam nemo delectus fugiat quas aut minima commodi. Placeat quaerat iste pariatur ex fuga aspernatur sit, earum consequuntur est, inventore nobis quisquam eos hic doloremque nesciunt tempora cum voluptatem repellat.'
 ]
-
 let quantidadeTexto = document.querySelector('#quantidade_txt');
 let tamanhoTexto = document.querySelector('#tamanho_txt')
 let opcoesTamanhoTexto = document.querySelectorAll("#tamano_txt > option").length
 let saida = document.querySelector('#saida_txt')
-//↑↑ CONFIGURAÇÕES
+let btnCopiarSaida = document.querySelector('#copiar_saida_txt')
+
+//↓↓ ATALHOS DE TECLADO
+window.addEventListener('keydown', tecla => {if(tecla.code == 'KeyL' && tecla.altKey == true){abrirPopup('#popup_log_novidades')}})
 
 //↓↓ ACIONADORES
 document.querySelector('#alteraTemaPagina').addEventListener('click', ()=>{alterarTema(1)})
+
 document.querySelector('#icone_Ajustes').addEventListener('click', ()=>{abrirPopup('#popup_ajustes')})
+
 document.querySelector('#icone_Info').addEventListener('click', ()=>{abrirPopup('#popup_sobre')})
-document.querySelector('#gerar_lorem').addEventListener('click', ()=>{
-    quantidadeTexto = document.querySelector('#quantidade_txt');
-    tamanhoTexto = document.querySelector('#tamanho_txt');
-    gerarLorem(quantidadeTexto.value, tamanhoTexto.value);
-})
+
+btnCopiarSaida.addEventListener('click', () => {saida = document.querySelector('#saida_txt'); navigator.clipboard.writeText(saida.value); btnCopiarSaida.innerHTML = 'Copiado!'; setTimeout(()=>{btnCopiarSaida.innerHTML = 'Copiar'},1000)})
 
 /*
-quantidadeTexto.addEventListener('change', () => {if(quantidadeTexto = 1){for(i=0;i<opcoesTamanhoTexto;i++){let opcao = document.querySelectorAll("#tamanho_txt > option")[i]; opcao.innerHTML = opcao.innerHTML.substring(0, opcao.innerHTML.length - 1)}} else {}})
+quantidadeTexto.addEventListener('change', ()=>{
+    if(quantidadeTexto.value == 1){
+        console.log(1)
+        
+
+        
+    }
+    else{
+        console.log(2)
+        if(tamanhoTexto.options[0].outerText.charAt(tamanhoTexto.value.length - 1) === 's'){} else{for(i=0;i<Number(opcoesTamanhoTexto);i++){document.querySelector(`#tamanho_txt > option:nth-child(${i+1})`).innerText =+ 's'}}
+    }
+})
 */
 
+//↓↓ LINKS EM POPUPS
+document.querySelector('#link_github_popup').addEventListener('click', function(){setTimeout(() => {
+    window.open('https://github.com/andremourasantos/loremipsum', '_blank')
+}, 125);})
+
+//↓↓ FERRAMENTA
+document.querySelector('#adicionarQuantidadeTxt').addEventListener('click', ()=>{quantidadeTexto.stepUp()})
+document.querySelector('#removerQuantidadeTxt').addEventListener('click', ()=>{quantidadeTexto.stepDown()})
+
+
+document.querySelector('#gerar_lorem').addEventListener('click', ()=>{quantidadeTexto = document.querySelector('#quantidade_txt'); tamanhoTexto = document.querySelector('#tamanho_txt'); gerarLorem(quantidadeTexto.value, tamanhoTexto.value);})
 
 function gerarLorem(quantidade,tamanho) {
     const n = Array.from(new Array(Number(quantidade)),Math.random).map(value => {return Number((value*10).toString().split('.',1))})
